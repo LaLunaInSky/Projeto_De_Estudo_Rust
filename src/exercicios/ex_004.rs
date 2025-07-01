041fa90a6acebfde2c66df7f444066c44841444b
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-01 20:57:45 -0300

    01/07/2025 20:57

--- src/exercicios/ex_004.rs
+++ src/exercicios/ex_004.rs
@@ -1,12 +1,15 @@
 use std::{
-    process::Command
+    process::Command,
+    io,
+    thread,
+    time::Duration
 };
 
 fn clean_terminal_linux() {
     Command::new("clear").status().unwrap();
 }
 
-fn descrição_do_exercícios() {
+fn descrição_do_exercício() {
     println!("Descrição do exercício 004:");
     println!(
         " Um programa que lê um número inteiro e\nmostra o seu dobro, triplo e a raiz\nquadrada."
@@ -16,7 +19,72 @@ fn descrição_do_exercícios() {
 pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     println!("{}", cabeçalho_do_programa);
 
-    descrição_do_exercícios();
+    descrição_do_exercício();
 
     println!();
+
+    let número_do_input = obter_input_de_um_número_inteiro(&cabeçalho_do_programa);
+
+    thread::sleep(Duration::from_millis(2000));
+
+    analisador_deo_número_inteiro(&número_do_input);
+}
+
+fn obter_input_de_um_número_inteiro(cabeçalho_do_programa: &String) -> u32 {
+    loop {
+        println!("Digite um número inteiro:");
+
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                match input.trim().parse::<u32>() {
+                    Ok(number) => {
+                        clean_terminal_linux();
+
+                        println!("{}", cabeçalho_do_programa);
+
+                        descrição_do_exercício();
+
+                        println!();
+
+                        println!("Analisando o número digitado {}...", number);
+
+                        return number;
+                    }
+                    Err(_) => {
+                        clean_terminal_linux();
+
+                        println!("{}", cabeçalho_do_programa);
+
+                        descrição_do_exercício();
+
+                        println!();
+
+                        println!("Erro! Entrada digitada não é válida!\n");
+                    }
+                }
+            }
+            Err(error) => println!("Error: {}", error),
+        }
+    }
+}
+
+fn analisador_deo_número_inteiro(número_inteiro: &u32) {
+    println!();
+
+    println!(
+        "O Dobro é..........: {}",
+        número_inteiro
+    );
+
+    println!(
+        "O triplo é.........: {}",
+        número_inteiro
+    );
+
+    println!(
+        "A Raiz Quadrada é..: {}",
+        número_inteiro
+    );
 }
