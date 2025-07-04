671f836ac917cf3d1484628dc5e8de05c6a22e8b
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-04 11:00:20 -0300

    04/07/2025 11:00

--- src/exercicios/ex_008.rs
+++ src/exercicios/ex_008.rs
@@ -1,4 +1,5 @@
 use std::{
+    io,
     process::Command
 };
 
@@ -21,4 +22,28 @@ pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     descrição_do_exercício();
 
     println!();
+
+    obter_a_quantia_de_dinheiro_na_carteira();
+}
+
+fn obter_a_quantia_de_dinheiro_na_carteira() {
+    println!("Quanto de dinheiro você tem na carteira?");
+
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            match input.trim().parse::<f32>() {
+                Ok(number) => {
+                    if number > 0.0 {
+                        println!("Number: R${:.2}", number);
+                    } else {
+                        println!("Erro! Valor inválido!");
+                    }
+                }
+                Err(_) => println!("Erro!"),
+            }
+        }
+        Err(_) => println!("Erro!"),
+    } 
 }
