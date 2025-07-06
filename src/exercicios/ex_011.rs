dd6f2adaf0ce411fbc8f650600aa16633521a8a1
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-06 14:09:31 -0300

    06/07/2025 14:09

--- src/exercicios/ex_011.rs
+++ src/exercicios/ex_011.rs
@@ -24,19 +24,34 @@ pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     println!();
 
     /* Corpo do exercício main */
-    obter_o_salario();
+    let salário_do_funcionário = obter_o_salario(&cabeçalho_do_programa);
+
+    calcular_o_aumento_do_salario(&salário_do_funcionário);
 
     /* Fim do Exercício */
-    // thread::sleep(Duration::from_millis(3000));
+    thread::sleep(Duration::from_millis(3000));
 
-    // println!("\nVoltando ao menu de exercícios...\n");
+    println!("\nVoltando ao menu de exercícios...\n");
 
-    // thread::sleep(Duration::from_millis(3000));  
+    thread::sleep(Duration::from_millis(3000));  
 
-    // clean_terminal_linux();  
+    clean_terminal_linux();  
 }
 
-fn obter_o_salario() {
+fn calcular_o_aumento_do_salario(salário: &f32) {
+    println!("Calculando o aumento de 15%...\n");
+
+    thread::sleep(Duration::from_millis(2000));
+
+    let novo_salário = salário + (salário * (15.0 / 100.0));
+
+    println!(
+        "O novo salário é de R${:.2}.",
+        novo_salário
+    );
+}
+
+fn obter_o_salario(cabeçalho_do_programa: &String) -> f32 {
     loop {
         println!("Digite o salário do funcionário:");
     
@@ -45,8 +60,45 @@ fn obter_o_salario() {
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
                 match input.trim().parse::<f32>() {
-                    Ok(number) => println!("Number: {}", number),
-                    Err(_) => println!("Erro! Digite um valor válido!"),
+                    Ok(salário) => {
+                        if salário > 0.0 {
+                            clean_terminal_linux();
+    
+                            println!("{}", cabeçalho_do_programa);
+    
+                            descrição_do_exercício();
+
+                            let salário_formatado = format!(
+                                "{:.2}", salário
+                            );
+
+                            match salário_formatado.parse::<f32>() {
+                                Ok(salário_final) => {
+                                    println!("\nO salário do funcionário é R${:.2}\n", salário_final);
+                                
+                                    return salário_final;
+                                }
+                                Err(_) => println!("Erro!"),
+                            }
+                        } else {
+                            clean_terminal_linux();
+
+                            println!("{}", cabeçalho_do_programa);
+
+                            descrição_do_exercício();
+
+                            println!("\nErro! Digite um valor maior que zero!\n");
+                        }
+                    }
+                    Err(_) => {
+                        clean_terminal_linux();
+
+                        println!("{}", cabeçalho_do_programa);
+
+                        descrição_do_exercício();
+
+                        println!("\nErro! Digite um valor válido!\n");
+                    }
                 }
             }
             Err(_) => println!("Erro!"),
