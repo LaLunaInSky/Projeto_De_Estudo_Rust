eb8ba1134c87a8d3cb9c742cc54d3c2b6abab768
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-11 14:13:40 -0300

    11/07/2025 14:13

--- src/exercicios/ex_021.rs
+++ src/exercicios/ex_021.rs
@@ -24,21 +24,49 @@ pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     println!();
 
     /* Corpo do Exercício - fn main */
-    obter_o_nome_de_uma_cidade(&cabeçalho_do_programa);
+    let nome_da_cidade = obter_o_nome_de_uma_cidade(&cabeçalho_do_programa);
+
+    analisar_o_nome_da_cidade(&nome_da_cidade);
 
     /* Fim de Exercício */
-    // thread::sleep(Duration::from_millis(3000));
+    thread::sleep(Duration::from_millis(3000));
 
-    // println!(
-    //     "\nVoltando ao menu de exercícios...\n"
-    // );
+    println!(
+        "\nVoltando ao menu de exercícios...\n"
+    );
 
-    // thread::sleep(Duration::from_millis(3000));
+    thread::sleep(Duration::from_millis(3000));
 
-    // clean_terminal_linux();
+    clean_terminal_linux();
 }
 
-fn obter_o_nome_de_uma_cidade(cabeçalho_do_programa: &String) {
+fn analisar_o_nome_da_cidade(nome_da_cidade: &String) {
+    thread::sleep(Duration::from_millis(2000));
+
+    println!("Analisando a cidade...\n");
+
+    thread::sleep(Duration::from_millis(2500));
+
+    let nome_da_cidade = nome_da_cidade.to_lowercase();
+
+    let nome_da_cidade_separado: Vec<&str> = nome_da_cidade.split(" ").collect();
+
+    let mut resposta_da_analise = String::from("não ");
+
+    if nome_da_cidade_separado[0].contains("santo") {
+        resposta_da_analise = String::from("")
+    }
+
+    println!(
+        "A cidade {},\n{}começa com \"SANTO\"!",
+        nome_da_cidade.to_uppercase(),
+        resposta_da_analise
+    );
+
+    thread::sleep(Duration::from_millis(3000));
+}
+
+fn obter_o_nome_de_uma_cidade(cabeçalho_do_programa: &String) -> String {
     loop {
         println!("Digite o nome de um cidade:");
 
@@ -46,7 +74,18 @@ fn obter_o_nome_de_uma_cidade(cabeçalho_do_programa: &String) {
 
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
-                println!("Input: {}", input);
+                clean_terminal_linux();
+
+                println!("{}", cabeçalho_do_programa);
+
+                descrição_do_exercício();
+
+                println!(
+                    "\nA cidade {},\nfoi adicionada com sucesso!\n",
+                    input.trim()
+                );
+
+                return (input.trim()).to_string();
             }
             Err(_) => println!("Erro!"),
         }
