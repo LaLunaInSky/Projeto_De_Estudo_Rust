5dcd8a66454946c6b124883ebe0282018d9f9d90
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-12 18:57:21 -0300

    12/07/2025 18:57

--- src/exercicios/ex_024.rs
+++ src/exercicios/ex_024.rs
@@ -29,7 +29,9 @@ pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     println!();
 
     /* Corpo do Exercício - fn main */
+    let nome_completo_digitado = obter_nome_completo_da_pessoa(&cabeçalho_do_programa);
 
+    analisar_o_nome_informado(&nome_completo_digitado);
 
     /* Fim do Exercício */
     // sleep(Duration::from_millis(3000));
@@ -41,4 +43,58 @@ pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
     // sleep(Duration::from_millis(3000));
 
     // clean_terminal_linux();
+}
+
+fn analisar_o_nome_informado(nome_informado: &String) {
+    let mut primeiro_nome = String::from("-");
+    let mut último_nome = String::from("-");
+
+    let nome_separado: Vec<&str> = nome_informado.split(" ").collect();
+
+    sleep(Duration::from_millis(1200));
+
+    println!("Analisando o nome...\n");
+
+    sleep(Duration::from_millis(2500));
+
+    println!(
+        "Primeiro Nome: {}",
+        primeiro_nome
+    );
+
+    sleep(Duration::from_millis(900));
+
+    println!(
+        "Último Nome..: {}",
+        último_nome
+    );
+
+    sleep(Duration::from_millis(1500));
+
+}
+
+fn obter_nome_completo_da_pessoa(cabeçalho_do_programa: &String) -> String {
+    loop {
+        println!("Digite o nome completo de uma pessoa:");
+
+        let mut input = String::new();
+    
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                clean_terminal_linux();
+
+                println!("{}", cabeçalho_do_programa);
+
+                descrição_do_exercício();
+
+                println!(
+                    "\nO nome {},\nfoi adicionado com sucesso!\n",
+                    input.trim()
+                );
+
+                return (input.trim()).to_string();
+            }
+            Err(_) => println!("Erro!"),
+        }
+    }
 }
