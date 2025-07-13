fd814c4e8689efc23869c7b6317ab65a67232a3b
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-12 21:21:53 -0300

    12/07/2025 21:21

--- src/exercicios.rs
+++ src/exercicios.rs
@@ -22,9 +22,12 @@ mod ex_021;
 mod ex_022;
 mod ex_023;
 mod ex_024;
+mod ex_025;
 
 pub fn executar_o_exercício_x(número_do_exercício: u32, cabeçalho_do_programa: &String) {
-    if número_do_exercício == 24 {
+    if número_do_exercício == 25 {
+        ex_025::rodar_o_exercício(&cabeçalho_do_programa);
+    } else if número_do_exercício == 24 {
         ex_024::rodar_o_exercício(&cabeçalho_do_programa);
     } else if número_do_exercício == 23 {
         ex_023::rodar_o_exercício(&cabeçalho_do_programa);
---
+++ src/exercicios/ex_025.rs
@@ -0,0 +1,39 @@
+use std::{
+    io,
+    thread::sleep,
+    time::Duration,
+    process::Command
+};
+
+fn clean_terminal_linux() {
+    Command::new("clear").status().unwrap();
+}
+
+fn descrição_do_exercício() {
+    println!("Descrição do exercício 025:");
+    println!(
+        " Um programa que faça o computador \"pensar\"\nem um número inteiro entre 0 e 5 e peça\npara o usuário tentar descobrir qual foi o\nnúmero escolhido pelo computador.\n O programa deverá escrever na tela se o\nusuário venceu ou perdeu."
+    );
+}
+
+pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
+    println!("{}", cabeçalho_do_programa);
+
+    descrição_do_exercício();
+
+    println!();
+
+    /* Corpo do Exercíco - fn main */
+
+
+    /* Fim do Exercício */
+    // sleep(Duration::from_millis(3000));
+
+    // println!(
+    //     "\nVoltando ao menu de exercícios...\n"
+    // );
+
+    // sleep(Duration::from_millis(3000));
+
+    // clean_terminal_linux();
+}
