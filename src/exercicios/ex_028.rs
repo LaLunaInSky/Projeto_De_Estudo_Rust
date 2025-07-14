1d35b55ec2e5599e0aedb0b19916ee105f1120ad
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-14 15:51:31 -0300

    14/07/2025 15:51

--- src/exercicios/ex_028.rs
+++ src/exercicios/ex_028.rs
@@ -17,24 +17,141 @@ fn descrição_do_exercício() {
 }
 
 pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
-    /* Começo do Exercício */
-    println!("{}", cabeçalho_do_programa);
+    loop {
+        /* Começo do Exercício */
+        println!("{}", cabeçalho_do_programa);
+    
+        descrição_do_exercício();
+    
+        println!();
+    
+        /* Corpo do Exercício - fn main */
+        let distância_da_viagem = obter_a_distância_da_viagem(&cabeçalho_do_programa);
+    
+        calcular_o_preço_da_viagem(&distância_da_viagem);
+    
+        let resposta_sobre_continuar_o_exercício = perguntar_se_quer_recomeçar_o_exercício(&cabeçalho_do_programa);
 
-    descrição_do_exercício();
-
-    println!();
-
-    /* Corpo do Exercício - fn main */
-
+        if resposta_sobre_continuar_o_exercício == false {
+            break;
+        }
+    }    
 
     /* Fim do Exercício */
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(3000));
 
-    // println!(
-    //     "\nVoltando ao menu de exercícios...\n"
-    // );
+    println!(
+        "\nVoltando ao menu de exercícios...\n"
+    );
 
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(3000));
 
-    // clean_terminal_linux();
+    clean_terminal_linux();
+}
+
+fn perguntar_se_quer_recomeçar_o_exercício(cabeçalho_do_programa: &String) -> bool {
+    loop {
+        println!("Quer calcular o preço de outra viagem?\n[S/N]");
+
+        let mut input = String::new();
+    
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let resposta_da_pergunta = input.trim().to_lowercase();
+
+                if resposta_da_pergunta == "s" || resposta_da_pergunta == "n" {
+                    if resposta_da_pergunta == "s" {
+                        clean_terminal_linux();
+
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    clean_terminal_linux();
+
+                    println!("{}", cabeçalho_do_programa);
+
+                    descrição_do_exercício();
+
+                    println!(
+                        "\nErro! Digite apenas S [Sim] ou N [Não]!\n"
+                    );
+                }
+            }
+            Err(_) => println!("Erro!"),
+        }
+    }
+}
+
+fn calcular_o_preço_da_viagem(distância_da_viagem: &f32) {
+    let mut valor_da_viagem: f32 = 0.0;
+
+    if *distância_da_viagem <= 200.0 {
+        valor_da_viagem = *distância_da_viagem * 0.50;
+    } else {
+        valor_da_viagem = *distância_da_viagem * 0.45;
+    }
+
+    sleep(Duration::from_millis(1500));
+
+    println!("Calculando o preço da viagem...\n");
+
+    sleep(Duration::from_millis(2500));
+
+    println!(
+        "O preço final é de R${:.2}.\n",
+        valor_da_viagem
+    );
+
+    sleep(Duration::from_millis(1500));
+}
+
+fn obter_a_distância_da_viagem(cabeçalho_do_programa: &String) -> f32 {
+    loop {
+        println!("Digite a distância da viagem em kms:");
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+
+            Ok(_) => {
+                match input.trim().parse::<f32>() {
+                    Ok(distância) => {
+
+                        let distância_formatada = format!(
+                            "{:.2}", distância
+                        );
+
+                        match distância_formatada.parse::<f32>() {
+                            Ok(distância_final) => {
+                                clean_terminal_linux();
+
+                                println!("{}", cabeçalho_do_programa);
+
+                                descrição_do_exercício();
+
+                                println!(
+                                    "\nA distância de {:.2}km,\nfoi adicionada com sucesso!\n",
+                                    distância_final
+                                );
+
+                                return distância_final;
+                            }
+                            Err(_) => println!("Erro!"),
+                        }
+                    }
+                    Err(_) => {
+                        clean_terminal_linux();
+
+                        println!("{}", cabeçalho_do_programa);
+
+                        descrição_do_exercício();
+
+                        println!("\nErro! Digite um valor válido!\n");
+                    }
+                }
+            }
+            Err(_) => println!("Erro!"),
+        }
+    }
 }
