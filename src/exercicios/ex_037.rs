17effc1c5a4abf5b62bb001402bd3852c41c69ad
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-22 16:03:14 -0300

    22/07/2025 16:03

---
+++ src/exercicios/ex_037.rs
@@ -0,0 +1,43 @@
+use std::{
+    io::stdin,
+    thread::sleep,
+    time::Duration,
+    process::Command
+};
+
+fn clean_terminal_linux() {
+    Command::new("clear").status().unwrap();
+}
+
+fn descrição_do_exercício() {
+    println!("Descrição do exercício 037:");
+    println!(
+        " Um programa que lê duas notas de um aluno\ne calcula sua média, mostrando uma\nmensagem no final, de acordo com a média\natingida:
+- Média abaixo de 5.0: Reprovado
+- Média entre 5.0 e 6.9: Recuperação
+- Média 7.0 ou superior: Aprovado"
+    );
+}
+
+pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
+    /* Começo do Exercício */
+    println!("{}", cabeçalho_do_programa);
+
+    descrição_do_exercício();
+
+    println!();
+
+    /* Corpo do Exercício - fn main */
+
+
+    /* Fim do Exercício */
+    // sleep(Duration::from_millis(3000));
+
+    // println!(
+    //     "\nVoltando ao menu de exercícios...\n"
+    // );
+
+    // sleep(Duration::from_millis(3000));
+
+    // clean_terminal_linux();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io,
+    io::stdin,
     thread::sleep,
     time::Duration,
     process::Command,
@@ -7,7 +7,7 @@ use std::{
 
 mod exercicios;
 
-fn construir_a_lista_de_exercícios(cabeçalho_do_programa: &String, total_de_exercícios: u32) -> u32 {
+fn construir_a_lista_de_exercícios(cabeçalho_do_programa: &String, total_de_exercícios: &u32) {
     let mut nome_de_todos_os_exercícios = vec![];
 
     for quantidade_de_exercícios in 1..(total_de_exercícios + 1) {
@@ -28,9 +28,6 @@ fn construir_a_lista_de_exercícios(cabeçalho_do_programa: &String, total_de_ex
         nome_de_todos_os_exercícios.push(nome);
     }
 
-    let tamanho_da_lista_de_exercícios = nome_de_todos_os_exercícios.len().to_string();
-    let tamanho_da_lista_de_exercícios: u32 = tamanho_da_lista_de_exercícios.parse().unwrap();
-
     println!("{}", cabeçalho_do_programa);
     
     println!("          Lista de Exercícios\n");
@@ -46,15 +43,14 @@ fn construir_a_lista_de_exercícios(cabeçalho_do_programa: &String, total_de_ex
     println!(
         "\n\n(Escreva SAIR para fechar o programa)\n\"Coloque APENAS o número do exercício\"\n"
     );
-
-    return tamanho_da_lista_de_exercícios;
 }
 
 fn menu_de_opções_de_exercícios(cabeçalho_do_programa: &String, total_de_exercícios: u32) {
     let mut complemento_da_pergunta = String::new();
-
+    
     loop {
-        let tamanho_da_lista_de_exercícios = construir_a_lista_de_exercícios(&cabeçalho_do_programa, total_de_exercícios);    
+
+        construir_a_lista_de_exercícios(&cabeçalho_do_programa, &total_de_exercícios);    
 
         println!(
             "{}Qual exercício você escolhe?",
@@ -63,7 +59,7 @@ fn menu_de_opções_de_exercícios(cabeçalho_do_programa: &String, total_de_exe
     
         let mut input = String::new();
     
-        match io::stdin().read_line(&mut input) {
+        match stdin().read_line(&mut input) {
             Ok(_) => {
                 if input.trim().to_lowercase() == "sair" {
                     break;
@@ -71,18 +67,22 @@ fn menu_de_opções_de_exercícios(cabeçalho_do_programa: &String, total_de_exe
 
                 match input.trim().parse::<u32>() {
                     Ok(number) => {
-                        if number > 0 && number <= tamanho_da_lista_de_exercícios {
+                        if number >= 1 && number <= total_de_exercícios {
                             clean_terminal_linux();
                             
                             println!("{}", cabeçalho_do_programa);
                             
                             println!("\nAbrindo o exercício {}...\n", number);
-
+    
                             sleep(Duration::from_millis(2000));
+    
+                            clean_terminal_linux();
+    
+                            exercicios::executar_o_exercício_x(number, &cabeçalho_do_programa);
+
+                            complemento_da_pergunta = String::new();
 
                             clean_terminal_linux();
-
-                            exercicios::executar_o_exercício_x(number, &cabeçalho_do_programa);
                         } else {
                             clean_terminal_linux();
 
@@ -113,7 +113,7 @@ fn main() {
     clean_terminal_linux();
 
     /* Alterar para o número do último exercício em modelagem */
-    let total_de_exercícios: u32 = 36;
+    let total_de_exercícios: u32 = 37;
 
     /* Não precisa mexer */
     let último_exercício = &total_de_exercícios;
