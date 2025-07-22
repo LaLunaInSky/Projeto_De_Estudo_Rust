93b336dbc5e34f281b0f74b70437288ad27630d9
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-22 15:39:22 -0300

    22/07/2025 15:39

--- src/exercicios/ex_036.rs
+++ src/exercicios/ex_036.rs
@@ -20,14 +20,12 @@ O programa também deverá mostrar o tempo\nque falta ou que passou do prazo."
 }
 
 struct Pessoa {
-    _ano_de_nascimento: u32,
     idade: u32,
 }
 
 impl Pessoa {
     fn new(ano_de_nascimento: u32, ano_atual: &u32) -> Self {
         Self {
-            _ano_de_nascimento: ano_de_nascimento,
             idade: *ano_atual - ano_de_nascimento,
         }
     }
@@ -35,36 +33,107 @@ impl Pessoa {
 }
 
 pub fn rodar_o_exercício(cabeçalho_do_programa: &String) {
-    /* Começo do Exercício */
-    println!("{}", cabeçalho_do_programa);
+    loop {
+        /* Começo do Exercício */
+        println!("{}", cabeçalho_do_programa);
 
-    descrição_do_exercício();
+        descrição_do_exercício();
 
-    println!();
+        println!();
 
-    /* Corpo do Exercício - fn main */
-    let ano_atual = obter_o_ano_atual();
+        /* Corpo do Exercício - fn main */
+        let ano_atual = obter_o_ano_atual();
 
-    let dado_da_pessoa_01 = Pessoa::new(
-        obter_o_ano_de_nascimento(
-            &cabeçalho_do_programa,
+        let dado_da_pessoa_01 = Pessoa::new(
+            obter_o_ano_de_nascimento(
+                &cabeçalho_do_programa,
+                &ano_atual
+            ),
             &ano_atual
-        ),
-        &ano_atual
-    );
+        );
 
-    println!("\n{}", dado_da_pessoa_01.idade);
+        analisar_o_alistamento_militar_baseado_na_idade(&dado_da_pessoa_01.idade);
+
+        let resposta_sobre_continuar = perguntar_se_quer_digitar_outro_ano(&cabeçalho_do_programa);
+
+        if !resposta_sobre_continuar {
+            break;
+        }
+    }
 
     /* Fim do Exercício */
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(3000));
 
-    // println!(
-    //     "\nVoltando ao menu de exercícios...\n"
-    // );
+    println!(
+        "\nVoltando ao menu de exercícios...\n"
+    );
 
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(3000));
 
-    // clean_terminal_linux();
+    clean_terminal_linux();
+}
+
+fn perguntar_se_quer_digitar_outro_ano(cabeçalho_do_programa: &String) -> bool {
+    loop {
+        println!("Quer adicionar outro ano? [S/N]");
+
+        let mut input = String::new();
+
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                let resposta_da_pergunta = input.trim().to_lowercase();
+
+                let resposta_da_pergunta = resposta_da_pergunta.as_str();
+
+                match resposta_da_pergunta {
+                    "s" => {
+                        clean_terminal_linux();
+
+                        return true;
+                    }
+                    "n" => return false,
+                    _ => {
+                        clean_terminal_linux();
+
+                        println!("{}", cabeçalho_do_programa);
+
+                        descrição_do_exercício();
+
+                        println!(
+                            "\nErro! Apenas é aceito S [sim] ou N [não]!\n"
+                        )
+                    }
+                }
+            }
+            Err(_) => println!("Erro!"),
+        }
+    }
+}
+
+fn analisar_o_alistamento_militar_baseado_na_idade(idade_da_pessoa: &u32) {
+    sleep(Duration::from_millis(1000));
+
+    println!("\nAnalisando o alistamento militar...\n");
+
+    sleep(Duration::from_millis(2500));
+
+    if *idade_da_pessoa > 18 {
+        println!(
+            "Você já passou do ano do seu alistamento!\nJá se passou {}.\n",
+            idade_da_pessoa - 18
+        );
+    } else if *idade_da_pessoa < 18 {
+        println!(
+            "Você ainda não chegou no ano\ndo seu alistamento! Ainda falta{} {} ano{}!\n",
+            if 18 - idade_da_pessoa != 1 {"m"} else {""},
+            18 - idade_da_pessoa,
+            if 18 - idade_da_pessoa != 1 {"s"} else {""}
+        );
+    } else {
+        println!(
+            "Você está no ano do seu alistamento!\n"
+        );
+    }
 }
 
 fn obter_o_ano_de_nascimento(cabeçalho_do_programa: &String, ano_atual: &u32) -> u32 {
