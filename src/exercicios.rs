f07fb896d14c1780c6e63316d96c4a3fe0fbcbaa
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-24 00:22:37 -0300

    24/07/2025 00:22

--- src/exercicios.rs
+++ src/exercicios.rs
@@ -40,7 +40,9 @@ mod ex_039;
 mod ex_040;
 
 pub fn executar_o_exercício_x(número_do_exercício: u32, cabeçalho_do_programa: &String) {
-    if número_do_exercício == 39 {
+    if número_do_exercício == 40 {
+        ex_040::rodar_o_exercício(&cabeçalho_do_programa);
+    } else if número_do_exercício == 39 {
         ex_039::rodar_o_exercício(&cabeçalho_do_programa);
     } else if número_do_exercício == 38 {
         ex_038::rodar_o_exercício(&cabeçalho_do_programa);
--- src/exercicios/ex_040.rs
+++ src/exercicios/ex_040.rs
@@ -1,3 +1,48 @@
 use std::{
-    
+    io::stdin,
+    thread::sleep,
+    time::Duration,
+    process::Command  
 };
+
+fn clean_terminal_linux() {
+    Command::new("clear").status().unwrap();
+}
+
+fn descrição_do_exercício() {
+    println!("Descrição do exercício 040:");
+    println!(
+        " Desenvolva uma lógica que lê o peso e a\naltura de uma pessoa, calcula o seu IMC e\nmostra seu status corporal, de acordo com\na tabela abaixo:
+
+- Abaixo de  18.5: Abaixo do peso
+- Entre 18.5 e 25: Peso ideal
+- Entre 25 e 30: Sobrepeso
+- Entre 30 e 40: Obesidade
+- Acima de 40: Obesidade mórbida"
+    );
+}
+
+pub fn rodar_o_exercício(
+    cabeçalho_do_programa: &String
+) {
+    /* Começo do Exercício */
+    println!("{}", cabeçalho_do_programa);
+
+    descrição_do_exercício();
+
+    println!();
+
+    /* Corpo do Exercício - fn main */
+
+
+    /* Fim do Exercício */
+    // sleep(Duration::from_millis(3000));
+
+    // println!(
+    //     "\nVoltando ao menu de exercícios,,,\n"
+    // );
+
+    // sleep(Duration::from_millis(3000));
+
+    // clean_terminal_linux();
+}
