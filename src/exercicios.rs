54a09e7bad4579c2049a41c3473e2af5459c0dfc
LaLunaInSky <moonaflordapele@gmail.com>
2025-07-31 18:55:11 -0300

    31/07/2025 18:55

--- src/exercicios.rs
+++ src/exercicios.rs
@@ -43,9 +43,12 @@ mod ex_042;
 mod ex_043;
 mod ex_044;
 mod ex_045;
+mod ex_046;
 
 pub fn executar_o_exercício_x(número_do_exercício: u32, cabeçalho_do_programa: &String) {
-    if número_do_exercício == 45 {
+    if número_do_exercício == 46 {
+        ex_046::rodar_o_exercício(&cabeçalho_do_programa);
+    } else if número_do_exercício == 45 {
         ex_045::rodar_o_exercício(&cabeçalho_do_programa);
     } else if número_do_exercício == 44 {
         ex_044::rodar_o_exercício(&cabeçalho_do_programa);
--- src/exercicios/ex_045.rs
+++ src/exercicios/ex_045.rs
@@ -31,16 +31,51 @@ pub fn rodar_o_exercício(
     println!();
 
     /* Corpo do Exercício */
+    let números_ímpares: Vec<u32> = obter_números_ímpares_até_quinhentos_até_quinhentos();
 
+    somar_todos_os_números(
+        &números_ímpares
+    );
+
+    println!(
+        "{:?}", números_ímpares
+    );
 
     /* Fim do Exercício */
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(6000));
 
-    // println!(
-    //     "\nVoltando ao menu de exercícios...\n"
-    // );
+    println!(
+        "\nVoltando ao menu de exercícios...\n"
+    );
 
-    // sleep(Duration::from_millis(3000));
+    sleep(Duration::from_millis(3000));
 
-    // clean_terminal_linux();
+    clean_terminal_linux();
+}
+
+fn somar_todos_os_números(
+    números: &Vec<u32>
+) {
+    let mut soma_dos_números = 0;
+
+    for número in números {
+        soma_dos_números += número;
+    }
+
+    println!(
+        "\nA soma de todos os números é: {}.\n",
+        soma_dos_números
+    );
+}
+
+fn obter_números_ímpares_até_quinhentos_até_quinhentos() -> Vec<u32> {
+    let mut números_ímpares: Vec<u32> = vec![];
+
+    for número in 1..501 {
+        if número % 2 != 0 {
+            números_ímpares.push(número);
+        }
+    }
+
+    return números_ímpares;
 }
