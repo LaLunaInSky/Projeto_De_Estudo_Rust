e3a31798091b418aec4e1c8adbede32c3f44b99c
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-14 10:30:27 -0300

    14/08/2025 10:30

--- src/exercicios.rs
+++ src/exercicios.rs
@@ -55,9 +55,15 @@ mod ex_054;
 mod ex_055;
 mod ex_056;
 mod ex_057;
+mod ex_058;
 
 pub fn executar_o_exercício_x(número_do_exercício: u32, cabeçalho_do_programa: &String) {
-    if número_do_exercício == 57 {
+    if número_do_exercício == 58 {
+        ex_058::rodar_o_exercício(
+            &cabeçalho_do_programa
+        );
+
+    } else if número_do_exercício == 57 {
         ex_057::rodar_o_exercício(
             &cabeçalho_do_programa
         );
