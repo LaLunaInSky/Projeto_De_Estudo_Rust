aed3673c09c0d42b7ff788d3d0c5fd07f562ccac
LaLunaInSky <moonaflordapele@gmail.com>
2025-08-14 09:41:45 -0300

    14/08/2025 09:41

--- src/exercicios/ex_057.rs
+++ src/exercicios/ex_057.rs
@@ -17,6 +17,32 @@ fn descrição_do_exercício() -> String {
     )
 }
 
+#[derive(Debug)]
+struct PA {
+    número: u32,
+    razão: u32,
+    próximo_termo: u32,
+}
+
+impl PA {
+    fn new(
+        número: u32,
+        razão: u32
+    ) -> Self {
+        Self {
+            número,
+            razão,
+            próximo_termo: número + razão
+        }
+    }
+
+    fn buscar_o_próximo_termo(
+        &mut self
+    ) {
+        self.próximo_termo += self.razão;
+    }
+}
+
 pub fn rodar_o_exercício(
     cabeçalho_do_programa: &String
 ) {
@@ -28,7 +54,21 @@ pub fn rodar_o_exercício(
     );
 
     /* Corpo do Exercício */
+    let pa = PA::new(
+        obter_número_inteiro(
+            &cabeçalho_do_programa, 
+            "pa"
+        ),
+        obter_número_inteiro(
+            &cabeçalho_do_programa,
+            "razão"
+        )
+    );
 
+    println!(
+        "{:?}\n",
+        pa
+    );
 
     /* Fim do Exercício */
     // sleep(Duration::from_millis(3000));
@@ -40,4 +80,79 @@ pub fn rodar_o_exercício(
     // sleep(Duration::from_millis(3000));
 
     // clean_terminal_linux();
+}
+
+fn obter_número_inteiro(
+    cabeçalho_do_programa: &String,
+    nome_da_chamada: &str
+) -> u32 {
+    loop {
+        println!(
+            "Digite o número da {}:",
+            nome_da_chamada.to_uppercase()
+        );
+
+        let mut input = String::new();
+
+        match stdin().read_line(
+            &mut input
+        ) {
+            Ok(_) => {
+                match input.trim().parse::<u32>() {
+                    Ok(número) => {
+                        if número == 0 && nome_da_chamada == "razão" {
+                            let número: u32 = 1;
+
+                            clean_terminal_linux();
+
+                            println!(
+                                "{}\n{}",
+                                cabeçalho_do_programa,
+                                descrição_do_exercício()
+                            );
+
+                            println!(
+                                "A {} de {},\nfoi adicionada com sucesso!\n",
+                                nome_da_chamada.to_uppercase(),
+                                número
+                            );
+
+                            return número;
+                            
+                        } else {
+                            clean_terminal_linux();
+
+                            println!(
+                                "{}\n{}",
+                                cabeçalho_do_programa,
+                                descrição_do_exercício()
+                            );
+
+                            println!(
+                                "A {} de {},\nfoi adicionada com sucesso!\n",
+                                nome_da_chamada.to_uppercase(),
+                                número
+                            );
+
+                            return número;
+                        }
+                    }
+                    Err(_) => {
+                        clean_terminal_linux();
+
+                        println!(
+                            "{}\n{}",
+                            cabeçalho_do_programa,
+                            descrição_do_exercício()
+                        );
+
+                        println!(
+                            "Erro! Digite número inteiros!\n"
+                        );
+                    }
+                }
+            }
+            Err(_) => (),
+        }
+    }
 }
