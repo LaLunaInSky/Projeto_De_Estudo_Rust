c7406ebb829cb2307e9f538490d3d2a079cf6e85
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-16 15:14:05 -0300

    16/09/2025 15:14

--- src/exercicios/ex_067.rs
+++ src/exercicios/ex_067.rs
@@ -1,95 +1,19 @@
-// use std::{
-//     io::stdin,
-//     thread::sleep,
-//     time::Duration
-// };
+use std::{
+    thread::sleep,
+    time::Duration
+};
 
 use crate::recursos::{
-    // limpar_terminal::limpar_terminal,
     descricao_de_exercicio::buscar_descrição_do_exercício,
     exercicio_informacoes::ExercícioInformações,
     perguntar_se_quer_iniciar_novamento_o_exercicio::perguntar_se_quer_iniciar_novamente_o_exercício,
     final_do_exercicio::rodar_final_do_exercício
 };
 
-struct TabelaDoBrasileirão2025 {
-    tabela_em_ordem_de_colocação: [String; 20],
-    cinco_primeiros_colocados: [String; 5],
-    quatro_últimos_colocados: [String; 4],
-    tabela_em_ordem_alfabética: [String; 20],
-    posição_do_time_chapecoense: i8
-}
+mod tabela_do_brasileirao_2025;
+mod ordenador_alfabetico_de_array;
 
-impl TabelaDoBrasileirão2025 {
-    fn new() -> Self {
-        let tabela_em_ordem_de_colocação: [String; 20] = [
-            String::from("flamengo"), String::from("cruzeiro"), String::from("palmeiras"), String::from("mirassol"),  String::from("bahia"), String::from("botafogo"), String::from("são paulo"), String::from("bragantino"), String::from("corinthians"), String::from("fluminense"), String::from("ceará"), String::from("internacional"), String::from("atlético-MG"), String::from("grêmio"), String::from("vasco"), String::from("santos"), String::from("vitória"), String::from("juventude"), String::from("fortaleza"), String::from("sport")
-        ];
-
-        let mut cinco_primeiros_colocados: [String; 5] = [
-            String::new(), String::new(), String::new(), String::new(), String::new()
-        ];
-
-        let mut quatro_últimos_colocados: [String; 4] = [
-            String::new(), String::new(), String::new(), String::new()
-        ];
-
-        let mut tabela_em_ordem_alfabética: [String; 20] = [
-            String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new(), String::new()
-        ];
-
-        let mut posição_do_time_chapecoense: i8 = -1;
-
-        for (
-            index, 
-            time
-        ) in tabela_em_ordem_de_colocação.iter().enumerate() {
-            if index <= 4 {
-                cinco_primeiros_colocados[index as usize] = time.clone();
-            } else if index >= 16 {
-                quatro_últimos_colocados[
-                    (index - 16) as usize
-                ] = time.clone()
-            }
-
-            if time == "chapecoense" {
-                posição_do_time_chapecoense = (index + 1) as i8;
-            }
-        }
-        
-        Self {
-            tabela_em_ordem_de_colocação,
-            cinco_primeiros_colocados,
-            quatro_últimos_colocados,
-            tabela_em_ordem_alfabética,
-            posição_do_time_chapecoense
-        }
-    }
-
-    fn get_tabela_em_ordem_de_colocação(
-        &self
-    ) -> [String; 20] {
-        return self.tabela_em_ordem_de_colocação.clone();
-    }
-
-    fn get_cinco_primeiros_colocados(
-        &self
-    ) -> [String; 5] {
-        return self.cinco_primeiros_colocados.clone();
-    }
-
-    fn get_quatro_últimos_colocados(
-        &self
-    ) -> [String; 4] {
-        return self.quatro_últimos_colocados.clone();
-    }
-
-    fn get_posição_do_time_chapecoense(
-        &self
-    ) -> i8 {
-        return self.posição_do_time_chapecoense;
-    }
-}
+use tabela_do_brasileirao_2025::TabelaDoBrasileirão2025;
 
 // 73!
 pub fn rodar_o_exercício(
@@ -107,6 +31,12 @@ pub fn rodar_o_exercício(
         exercício_informações.mostrar_informações();
 
         /* Corpo do Exercício */
+        let tabela_do_brasileirão_2025 = TabelaDoBrasileirão2025::new();
+
+        analisar_a_tabela(
+            &tabela_do_brasileirão_2025
+        );
+
         let resposta_da_pergunta = perguntar_se_quer_iniciar_novamente_o_exercício(
             &exercício_informações
         );
@@ -118,4 +48,48 @@ pub fn rodar_o_exercício(
 
     /* Fim do Exercício */
     rodar_final_do_exercício();
+}
+
+fn analisar_a_tabela(
+    tabela_do_brasileirão_2025: &TabelaDoBrasileirão2025
+) {
+    let analise_se_tem_o_time_chapecoense = if tabela_do_brasileirão_2025.get_posição_do_time_chapecoense() >= 0 {
+        format!(
+            "O time \"Chapecoense\" está na posição {}",
+            tabela_do_brasileirão_2025.get_posição_do_time_chapecoense()
+        )
+    } else {
+        format!(
+            "O time \"Chapecoense\" não está em nenhuma posição!"
+        )
+    };
+
+    sleep(Duration::from_millis(1000));
+
+    println!(
+        "Analisando a tabela..."
+    );
+
+    sleep(Duration::from_millis(1500));
+
+    println!(
+        "
+ {}
+
+1º - {}
+
+2º - {}
+
+3º - {}
+
+4º - {}
+",
+        tabela_do_brasileirão_2025.get_tabela_em_ordem_de_colocação(),
+        tabela_do_brasileirão_2025.get_cinco_primeiros_colocados(),
+        tabela_do_brasileirão_2025.get_quatro_últimos_colocados(),
+        tabela_do_brasileirão_2025.get_tabela_em_ordem_alfabética(),
+        analise_se_tem_o_time_chapecoense
+    );
+
+    sleep(Duration::from_millis(1100));
 }
---
+++ src/exercicios/ex_067/tabela_do_brasileirao_2025.rs
@@ -0,0 +1,206 @@
+use crate::exercicios::ex_067::ordenador_alfabetico_de_array::ordenador_alfabético_de_array;
+
+pub struct TabelaDoBrasileirão2025 {
+    tabela_em_ordem_de_colocação: [String; 20],
+    cinco_primeiros_colocados: [String; 5],
+    quatro_últimos_colocados: [String; 4],
+    tabela_em_ordem_alfabética: Vec<String>,
+    posição_do_time_chapecoense: i8
+}
+
+impl TabelaDoBrasileirão2025 {
+    pub fn new() -> Self {
+        let tabela_em_ordem_de_colocação: [String; 20] = [
+            String::from("flamengo"), String::from("cruzeiro"), String::from("palmeiras"), String::from("mirassol"),  String::from("bahia"), String::from("botafogo"), String::from("são paulo"), String::from("bragantino"), String::from("corinthians"), String::from("fluminense"), String::from("ceará"), String::from("internacional"), String::from("atlético-MG"), String::from("grêmio"), String::from("vasco"), String::from("santos"), String::from("vitória"), String::from("juventude"), String::from("fortaleza"), String::from("sport")
+        ];
+
+        let mut cinco_primeiros_colocados: [String; 5] = [
+            String::new(), String::new(), String::new(), String::new(), String::new()
+        ];
+
+        let mut quatro_últimos_colocados: [String; 4] = [
+            String::new(), String::new(), String::new(), String::new()
+        ];
+
+        let tabela_em_ordem_alfabética = ordenador_alfabético_de_array(
+            &tabela_em_ordem_de_colocação
+        );
+
+        let mut posição_do_time_chapecoense: i8 = -1;
+
+        for (
+            index, 
+            time
+        ) in tabela_em_ordem_de_colocação.iter().enumerate() {
+            if index <= 4 {
+                cinco_primeiros_colocados[index as usize] = time.clone();
+            } else if index >= 16 {
+                quatro_últimos_colocados[
+                    (index - 16) as usize
+                ] = time.clone()
+            }
+
+            if time == "chapecoense" {
+                posição_do_time_chapecoense = (index + 1) as i8;
+            }
+        }
+        
+        Self {
+            tabela_em_ordem_de_colocação,
+            cinco_primeiros_colocados,
+            quatro_últimos_colocados,
+            tabela_em_ordem_alfabética,
+            posição_do_time_chapecoense
+        }
+    }
+
+    pub fn get_tabela_em_ordem_de_colocação(
+        &self
+    ) -> String {
+        let mut tabela_em_string: String = String::new();
+
+        for (
+            index,
+            time
+        ) in self.tabela_em_ordem_de_colocação.iter().enumerate() {
+            if index == (self.tabela_em_ordem_de_colocação.len() - 1) {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}.",
+                        time
+                    ).as_str()
+                );
+            } else if index != 0 && index % 3 == 0 {
+                tabela_em_string.push_str(
+                    format!(
+                        "{},\n",
+                        time
+                    ).as_str()
+                );
+            } else {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}, ",
+                        time
+                    ).as_str()
+                );
+            }
+        }
+
+        return tabela_em_string;
+    }
+
+    pub fn get_cinco_primeiros_colocados(
+        &self
+    ) -> String {
+        let mut tabela_em_string = String::new();
+
+        for (
+            index,
+            time
+        ) in self.cinco_primeiros_colocados.iter().enumerate() {
+            if index == (self.cinco_primeiros_colocados.len() - 1) {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}.",
+                        time
+                    ).as_str()
+                );
+            } else if index != 0 && index % 2 == 0 {
+                tabela_em_string.push_str(
+                    format!(
+                        "{},\n",
+                        time
+                    ).as_str()
+                );
+            } else {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}, ",
+                        time
+                    ).as_str()
+                );
+            }
+        }
+
+        return tabela_em_string;
+    }
+
+    pub fn get_quatro_últimos_colocados(
+        &self
+    ) -> String {
+                let mut tabela_em_string = String::new();
+
+        for (
+            index,
+            time
+        ) in self.quatro_últimos_colocados.iter().enumerate() {
+            if index == (self.quatro_últimos_colocados.len() - 1) {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}.",
+                        time
+                    ).as_str()
+                );
+            } else if index != 0 && index % 2 == 0 {
+                tabela_em_string.push_str(
+                    format!(
+                        "{},\n",
+                        time
+                    ).as_str()
+                );
+            } else {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}, ",
+                        time
+                    ).as_str()
+                );
+            }
+        }
+
+        return tabela_em_string;
+    }
+
+    pub fn get_tabela_em_ordem_alfabética(
+        &self
+    ) -> String {
+                let mut tabela_em_string = String::new();
+
+        for (
+            index,
+            time
+        ) in self.tabela_em_ordem_alfabética.iter().enumerate() {
+            if index == (self.tabela_em_ordem_alfabética.len() - 1) {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}.",
+                        time
+                    ).as_str()
+                );
+            } else if index != 0 && index % 3 == 0 && index > 3 && index == 2 {
+                tabela_em_string.push_str(
+                    format!(
+                        "{},\n",
+                        time
+                    ).as_str()
+                );
+            } else {
+                tabela_em_string.push_str(
+                    format!(
+                        "{}, ",
+                        time
+                    ).as_str()
+                );
+            }
+        }
+
+        return tabela_em_string;
+    }
+
+    pub fn get_posição_do_time_chapecoense(
+        &self
+    ) -> i8 {
+        return self.posição_do_time_chapecoense;
+    }
+}
